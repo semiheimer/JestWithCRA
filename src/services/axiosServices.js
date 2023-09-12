9a1056daffe94de213a043a05feed34ab7cba322
semiheimer <skutlu@gmail.com>
2023-09-12 18:48:39 +0300

    add:husky1

--- src/services/axiosServices.js
+++ src/services/axiosServices.js
@@ -1,16 +1,17 @@
-import axios from "axios"
-const URL='https://jsonplaceholder.typicode.com/posts';
+import axios from 'axios';
+const URL = 'https://jsonplaceholder.typicode.com/posts';
 
-const getPosts=()=>{
-	return axios.get(URL).then((res)=>res.data)
-}
+const getPosts = () => {
+    return axios.get(URL).then(res => res.data);
+};
 
-const getPost=(id)=>{
-	return axios.get(`${URL}/${id}`).then((res)=>res.data)
-}
+const getPost = async id => {
+    return await axios.get(`${URL}/${id}`).then(res => res.data);
+};
 
- const axiosServices={
-	getPost,getPosts
-}
+const axiosServices = {
+    getPost,
+    getPosts,
+};
 
-export default axiosServices
+export default axiosServices;
